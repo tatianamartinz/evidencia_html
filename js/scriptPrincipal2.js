218d7d62490f23500d522a8d640f2466dc682ad9
“Mi <tamartinz@hotmail.com>
2020-11-25 13:31:07 -0600

    primera version

---
+++ js/scriptPrincipal2.js
@@ -0,0 +1,10 @@
+$(document).on("ready",configurarApp);
+
+function configurarApp()
+{
+	var canvas=document.getElementById("miCanvas");
+	var ctx=canvas.getContext("2d");
+	canvas.width=screen.availWidth;
+	
+
+}
